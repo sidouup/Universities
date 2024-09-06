249694b9cda07b7c1de03cfde036383c711d9a10
sidouup <160077558+sidouup@users.noreply.github.com>
2024-09-06 01:04:44 -0400

    Update universitesall.py

--- pages/universitesall.py
+++ pages/universitesall.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide", page_title="University Search Tool")
 # Add the image in the center of the page
 st.markdown("""
 <div style="text-align:center;">
-    <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1200,h=630,fit=crop,f=jpeg/YBgonz9JJqHRMK43/blue-red-minimalist-high-school-logo-10-ALpbw97ayrS4jgJX.png" 
+    <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=228,fit=crop,q=95/YBgonz9JJqHRMK43/blue-red-minimalist-high-school-logo-9-AVLN0K6MPGFK2QbL.png" 
     alt="Logo" style="width:50%; height:auto;">
 </div>
 """, unsafe_allow_html=True)
